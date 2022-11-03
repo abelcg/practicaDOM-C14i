9dd19ac84ce4bdf665b17722c4a6b586d221cd92
abelcg <abelcg86@gmail.com>
2022-11-03 12:58:14 -0300

    practica DOM

--- js/app.js
+++ js/app.js
@@ -19,18 +19,38 @@ const verMas = () => {
   if (btnVerMas.innerHTML === "Ver más") {
     //Opción 1 - camino largo
     //1- crear el elemento del html
-    let parrafo = document.createElement("p"); //crea la etiqueta <p></p>
+   // let parrafo = document.createElement("p"); //crea la etiqueta <p></p>
     //2- agregar la informacion al elemento html creado
-    parrafo.innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+  /*   parrafo.innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
          assumenda velit eligendi dolores eos earum in optio. Vel tenetur ab
          illum enim saepe hic repellendus. Iusto vitae pariatur quae molestiae
          magni harum, inventore quis porro ipsam quibusdam, laudantium nobis
          fugiat nisi, nihil corrupti dignissimos illum officiis eum autem ea
          reiciendis.`;
-    parrafo.className = "lead";
+    parrafo.className = "lead"; */
     //3- insertar el nuevo elemento en el DOM
-    contenedorPadre.appendChild(parrafo);
+    //contenedorPadre.appendChild(parrafo);
 
-    //Opción 2 - camino corto
-  }
+     //Opcion 2 - camino corto
+     contenedorPadre.innerHTML +=
+     '<p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio, aliquid? Tenetur sint accusamus nobis quas molestiae, aliquid itaque consequuntur esse. Tempore possimus temporibus eum unde dolor ad dolore. Beatae quo aliquam placeat necessitatibus odio repellendus blanditiis perferendis modi esse amet sit autem ipsa tempora, nulla quis possimus accusamus ab harum illum provident ducimus quam sed! Porro qui nostrum quis suscipit.</p>';
+
+   // modificar el texto del boton
+   btnVerMas.innerHTML = "Ocultar";
+   btnVerMas.className = "btn btn-danger";
+ } else {
+   console.log("aqui quiero borrar el parrafo");
+   console.log(contenedorPadre.hasChildNodes()); // devuelve true o false
+   console.log(contenedorPadre.children.length); //retorna un arreglo de nodos hijos
+   if(contenedorPadre.hasChildNodes() && contenedorPadre.children.length >= 2 ){
+       //   borrar el parrafo
+       contenedorPadre.removeChild(contenedorPadre.children[1]);
+       // modificar el texto y la estetica del boton
+       btnVerMas.innerHTML = 'Ver mas';
+       btnVerMas.className = 'btn btn-primary';
+   }
+
+ }
 };
+
+// la propiedad para acceder al texto de un input no es innerHTML se llama value
